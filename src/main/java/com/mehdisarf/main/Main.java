53a66bda3d50ef2b4e603ed4e0e02001ee6d4d83
M.Mehdi <m.sfj9696@gmail.com>
2021-11-13 04:48:00 +0330

    Referring to the Parrot instance from the context

--- src/main/java/com/mehdisarf/main/Main.java
+++ src/main/java/com/mehdisarf/main/Main.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
 
-        // instruct Spring to use my config class.
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+        // referring to the Parrot instance from the context
+        Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p.getName());
     }
 }
