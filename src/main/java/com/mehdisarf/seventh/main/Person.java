61d2c4d0d097ec9fbd55b400ff464c4b44746aaa
M.Mehdi <m.sfj9696@gmail.com>
2021-11-22 03:37:36 +0330

    Choose from multiple beans in the Spring context v4 (with @Autowired and @Qualifier)

--- src/main/java/com/mehdisarf/seventh/main/Person.java
+++ src/main/java/com/mehdisarf/seventh/main/Person.java
@@ -1,6 +1,7 @@
 package com.mehdisarf.seventh.main;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +11,8 @@ public class Person {
     private Parrot parrot;
 
     @Autowired
-    public Person(Parrot parrot2) {
-        this.parrot = parrot2;
+    public Person(@Qualifier("parrot2") Parrot par) {
+        this.parrot = par;
     }
 
     public String getName() {
