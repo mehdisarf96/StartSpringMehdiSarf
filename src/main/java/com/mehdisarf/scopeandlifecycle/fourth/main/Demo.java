eb769ecd8d06962b2cafec2b91890d329dc40d56
M.Mehdi <m.sfj9696@gmail.com>
2021-11-25 05:02:25 +0330

    Test lazy instantiation v2

--- src/main/java/com/mehdisarf/scopeandlifecycle/fourth/main/Demo.java
+++ src/main/java/com/mehdisarf/scopeandlifecycle/fourth/main/Demo.java
@@ -1,13 +1,21 @@
 package com.mehdisarf.scopeandlifecycle.fourth.main;
 
 import com.mehdisarf.scopeandlifecycle.fourth.config.ProjectConfig;
+import com.mehdisarf.scopeandlifecycle.fourth.services.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Demo {
-
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        System.out.println("<< Before retrieving the CommentService >>");
+
+        // where Spring needs to provide a reference to the CommentService bean,
+        // Spring also creates the instance.
+        CommentService cService = context.getBean(CommentService.class);
+
+        System.out.println("<< After retrieving the CommentService >>");
     }
 }
