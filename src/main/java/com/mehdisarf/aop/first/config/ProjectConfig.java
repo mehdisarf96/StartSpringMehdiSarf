1585e7103abaadbc581a62aacd8e325f84a0438d
M.Mehdi <m.sfj9696@gmail.com>
2021-11-28 17:56:43 +0330

    Implement a simple aspect.

--- src/main/java/com/mehdisarf/aop/first/config/ProjectConfig.java
+++ src/main/java/com/mehdisarf/aop/first/config/ProjectConfig.java
@@ -1,11 +1,19 @@
 package com.mehdisarf.aop.first.config;
 
+import com.mehdisarf.aop.first.aspects.LoggingAspect;
 import com.mehdisarf.aop.first.services.CommentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = "com.mehdisarf.aop.first")
+@EnableAspectJAutoProxy // Enable the aspect mechanism
 public class ProjectConfig {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
 }
